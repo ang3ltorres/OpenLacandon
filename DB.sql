1d68f1920199a0f57e77d3bade9bd3b6b9ca19fb
Th3UwU <angel.storres@alumnos.udg.mx>
2022-10-18 15:54:40 -0500

    DB Updated

--- DB.sql
+++ DB.sql
@@ -9,7 +9,6 @@ CREATE DATABASE OPENLACANDON
 	CONNECTION LIMIT = -1
 	TEMPLATE template0;
 
-
 -- CLIENT TABLE
 CREATE TABLE CLIENT
 (
@@ -40,6 +39,8 @@ INSERT INTO CLIENT VALUES(DEFAULT, 'ANGEL', '123', 'angel.storres@alumnos.udg.mx
 INSERT INTO CLIENT VALUES(DEFAULT, 'CHRISTIAN', '678', 'christian.delacruz1289@alumnos.udg.mx');
 INSERT INTO CLIENT VALUES(DEFAULT, 'DAMIAN', '567', 'angel.garcia9106@alumnos.udg.mx');
 INSERT INTO CLIENT VALUES(DEFAULT, 'STALIN', '123', 'stalin.mendoza7428@alumnos.udg.mx');
+INSERT INTO CLIENT VALUES(DEFAULT, 'MARIA', 'ABC56', 'maria.hernandez62@alumnos.udg.mx');
+INSERT INTO CLIENT VALUES(DEFAULT, 'JUAN', 'DFG12', 'juan.ramirez5412@alumnos.udg.mx');
 
 -- BOOK TABLE
 CREATE TABLE BOOK
@@ -50,6 +51,7 @@ CREATE TABLE BOOK
 	-- BOOK INFO
 	TITLE VARCHAR(64) NOT NULL,
 	AUTHOR VARCHAR(64) NOT NULL,
+	RELEASE_DATE SMALLINT NOT NULL,
 	GENRE VARCHAR(64) DEFAULT NULL,
 	SYNOPSIS VARCHAR(2048) DEFAULT NULL,
 	RATING SMALLINT DEFAULT 0 NOT NULL,
@@ -58,12 +60,11 @@ CREATE TABLE BOOK
 );
 
 -- BOOK EXAMPLES
-INSERT INTO BOOK VALUES(DEFAULT, 'The Martian Chronicles', 'Ray Bradbury', 'Ciencia ficción', 'In The Martian Chronicles, Ray Bradbury, Americas preeminent storyteller, imagines a place of hope, dreams, and metaphor-- of crystal pillars and fossil seas--where a fine dust settles on the great empty cities of a vanished, devastated civilization. Earthmen conquer Mars and then are conquered by it, lulled by dangerous lies of comfort and familiarity, and enchanted by the lingering glamour of an ancient, mysterious native race. In this classic work of fiction, Bradbury exposes our ambitions, weaknesses, and ignorance in a strange and breathtaking world where man does not belong.', 7);
-
-INSERT INTO BOOK VALUES(DEFAULT, 'Fairy Tale', 'Stephen King', 'Terror', 'Legendary storyteller Stephen King goes into the deepest well of his imagination in this spellbinding novel about a seventeen-year-old boy who inherits the keys to a parallel world where good and evil are at war, and the stakes could not be higher--for that world or ours.<br>Charlie Reade looks like a regular high school kid, great at baseball and football, a decent student. But he carries a heavy load. His mom was killed in a hit-and-run accident when he was seven, and grief drove his dad to drink. Charlie learned how to take care of himself--and his dad. When Charlie is seventeen, he meets a dog named Radar and her aging master, Howard Bowditch, a recluse in a big house at the top of a big hill, with a locked shed in the backyard. Sometimes strange sounds emerge from it.<br>Charlie starts doing jobs for Mr. Bowditch and loses his heart to Radar. Then, when Bowditch dies, he leaves Charlie a cassette tape telling a story no one would believe. What Bowditch knows, and has kept secret all his long life, is that inside the shed is a portal to another world.<br>Kings storytelling in Fairy Tale soars. This is a magnificent and terrifying tale in which good is pitted against overwhelming evil, and a heroic boy--and his dog--must lead the battle.<br>Early in the Pandemic, King asked himself: What could you write that would make you happy?<br>As if my imagination had been waiting for the question to be asked, I saw a vast deserted city--deserted but alive. I saw the empty streets, the haunted buildings, a gargoyle head lying overturned in the street. I saw smashed statues (of what I didnt know, but I eventually found out). I saw a huge, sprawling palace with glass towers so high their tips pierced the clouds. Those images released the story I wanted to tell.', 6);
-
-INSERT INTO BOOK VALUES(DEFAULT, 'Harry Potter and the Sorcerers Stone', 'J.K Rowling');
-INSERT INTO BOOK VALUES(DEFAULT, 'The Divine Comedy', 'Dante Alighieri');
+INSERT INTO BOOK VALUES(DEFAULT, 'The Martian Chronicles', 'Ray Bradbury', 1950, 'Ciencia ficción', 'Esta colección de relatos recoge la crónica de la colonización de Marte por parte de una humanidad que huye de un mundo al borde de la destrucción. Los colonos llevan consigo sus deseos más íntimos y el sueño de reproducir en el Planeta Rojo una civilización de perritos calientes, cómodos sofás y limonada en el porche al atardecer. Pero su equipaje incluye también los miedos ancestrales, que se traducen en odio a lo diferente, y las enfermedades que diezmarán a los marcianos.<br><br>Conforme a su concepción de lo que debe ser la ciencia ficción, Bradbury se traslada al futuro para iluminar el presente y explorar la naturaleza humana. Escritas en la década de los cuarenta, estas deslumbrantes e intensas historias constituyen un canto contra el racismo, la guerra y la censura, destilando nostalgia e idealismo.', 7);
+INSERT INTO BOOK VALUES(DEFAULT, 'Cuento de hadas', 'Stephen King', 2022, 'Fantasia', 'Charlie Reade parece un estudiante de instituto normal y corriente, pero carga con un gran peso sobre los hombros. Cuando él solo tenía diez años, su madre fue víctima de un atropello y la pena empujó a su padre a la bebida. Aunque era demasiado joven, Charlie tuvo que aprender a cuidarse solo... y también a ocuparse de su padre.<br><br>Ahora, con diecisiete años, Charlie encuentra dos amigos inesperados: una perra llamada Radar y Howard Bowditch, su anciano dueño. El señor Bowditch es un ermitaño que vive en una colina enorme, en una casa enorme que tiene un cobertizo cerrado a cal y canto en el patio trasero. A veces, sonidos extraños emergen de él.<br><br>Mientras Charlie se encarga de hacer recados para el señor Bowditch, Radar y él se hacen inseparables. Cuando el anciano fallece, le deja al chico una cinta de casete que contiene una historia increíble y el gran secreto que Bowditch ha guardado durante toda su vida: dentro de su cobertizo existe un portal que conduce a otro mundo.', 6);
+INSERT INTO BOOK VALUES(DEFAULT, 'Ubik', 'Philip K. Dick', 1969, 'Ciencia ficción', 'Glen Runciter está muerto. ¿O lo están todos los demás? Lo que es seguro es que alguien ha muerto en una explosión organizada por los competidores de Runciter. De hecho, sus empleados asisten a un funeral. Pero durante el duelo comienzan a recibir mensajes desconcertantes, e incluso morbosos, de su jefe. Y el mundo a su alrededor comienza a desmoronarse de un modo que sugiere que a ellos tampoco les queda mucho tiempo. Esta mordaz comedia metafísica de muerte y salvación (que podrá llevar en un cómodo envase) es un tour de force de amenaza paranoica y comedia absurda, en la cual los muertos ofrecen consejos comerciales, compran su siguiente reencarnación y corren el riesgo continuo de volver a morir.', 8);
+INSERT INTO BOOK VALUES(DEFAULT, '¿Sueñan los androides con ovejas eléctricas?', 'Philip K. Dick', 1968, 'Ciencia ficción', 'En el año 2021 la guerra mundial ha exterminado a millones de personas. Los supervivientes codician cualquier criatura viva, y aquellos que no pueden permitirse pagar por ellas se ven obligados a adquirir réplicas increíblemente realistas. Las empresas fabrican incluso seres humanos. Rick Deckard es un cazarrecompensas cuyo trabajo es encontrar androides rebeldes y retirarlos, pero la tarea no será tan sencilla cuando tenga que enfrentarse a los nuevos modelos Nexus-6, prácticamente indistinguibles de los seres humanos.', 9);
+INSERT INTO BOOK VALUES(DEFAULT, 'Fahrenheit 451', 'Ray Bradbury', 1953, 'Ciencia ficción', 'Fahrenheit 451. La temperatura a la que el papel se enciende y arde. Como 1984 de George Orwell, como Un mundo feliz de Aldous Huxley, Fahrenheit 451 describe una civilización occidental esclavizada por los media, los tranquilizantes y el conformismo.<br><br>La visión de Bradbury es asombrosamente profética: las pantallas de TV ocupan paredes y exhiben folletines interactivos, unos auriculares transmiten a todas horas una insípida corriente de música y noticias, en las avenidas los coches corren a 150 kilómetros por hora persiguiendo a peatones; y el cuerpo de bomberos auxiliado por el Sabueso Mecánico, rastrea y elimina a los disidentes que conservan y leen libros.', 7);
 
 -- FORMAT TABLE
 CREATE TABLE FORMAT
@@ -72,10 +73,12 @@ CREATE TABLE FORMAT
 	ID SMALLSERIAL PRIMARY KEY NOT NULL,
 
 	-- FOREIGN KEY
-	ISBN SMALLINT REFERENCES BOOK(ISBN) NOT NULL,
+	ISBN SMALLINT NOT NULL REFERENCES BOOK(ISBN)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
 
 	-- INFO
-	PRICE_LIST MONEY DEFAULT 0 NOT NULL,
+	PRICE_LIST REAL DEFAULT 0 NOT NULL,
 	TYPE VARCHAR(64) DEFAULT 'Kindle' NOT NULL,
 	PAGES SMALLINT DEFAULT 0 NOT NULL,
 	STOCK SMALLINT DEFAULT -1 NOT NULL
@@ -85,9 +88,8 @@ CREATE TABLE FORMAT
 INSERT INTO FORMAT VALUES(DEFAULT, 1, 90, 'Kindle', 140, -1);
 INSERT INTO FORMAT VALUES(DEFAULT, 1, 265, 'Pasta blanda', 120, 55);
 INSERT INTO FORMAT VALUES(DEFAULT, 1, 640, 'Pasta dura', 175, 30);
-INSERT INTO FORMAT VALUES(DEFAULT, 2, 250, 'Kindle', 234, -1);
-INSERT INTO FORMAT VALUES(DEFAULT, 2, 460, 'Pasta blanda', 340, 70);
-INSERT INTO FORMAT VALUES(DEFAULT, 2, 620, 'Pasta dura', 280, 45);
+INSERT INTO FORMAT VALUES(DEFAULT, 2, 120, 'Kindle', 230, -1);
+INSERT INTO FORMAT VALUES(DEFAULT, 2, 640, 'Pasta dura', 190, 30);
 INSERT INTO FORMAT VALUES(DEFAULT, 3, 370, 'Pasta dura', 490, 90);
 INSERT INTO FORMAT VALUES(DEFAULT, 4, 120, 'Kindle', 385, -1);
 
@@ -98,7 +100,9 @@ CREATE TABLE ORD
 	ID SMALLSERIAL PRIMARY KEY NOT NULL,
 
 	-- FOREIGN KEY
-	ID_CLIENT SMALLINT REFERENCES CLIENT(ID) NOT NULL,
+	ID_CLIENT SMALLINT NOT NULL REFERENCES CLIENT(ID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
 
 	-- INFO
 	DATE_ORDER DATE DEFAULT NOW() NOT NULL,
@@ -106,4 +110,91 @@ CREATE TABLE ORD
 );
 
 -- ORDER EXAMPLES
-INSERT INTO ORD VALUES(DEFAULT, 1, DEFAULT, '2022-09-29');
+INSERT INTO ORD VALUES(DEFAULT, 4, 2, DEFAULT, '2022-12-29');
+INSERT INTO ORD VALUES(DEFAULT, 5, 7, DEFAULT, '2022-12-29');
+
+-- ORDER_DETAIL TABLE
+CREATE TABLE ORD_DETAIL
+(
+	-- PRIMARY KEY
+	ID SMALLSERIAL PRIMARY KEY NOT NULL,
+
+	-- FOREIGN KEY
+	ID_ORDER SMALLINT NOT NULL REFERENCES ORD(ID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+
+	ID_FORMAT SMALLINT NOT NULL REFERENCES FORMAT(ID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+
+	-- INFO
+	PRICE_SOLD MONEY NOT NULL,
+	AMOUNT SMALLINT NOT NULL
+);
+
+-- ORDER_DETAIL EXAMPLES
+INSERT INTO ORD_DETAIL VALUES(DEFAULT, 1, 5, (SELECT PRICE_LIST FROM FORMAT WHERE ID = 5), 2);
+
+-- SUPPLIER TABLE
+CREATE TABLE SUPPLIER
+(
+	-- PRIMARY KEY
+	ID SMALLSERIAL PRIMARY KEY NOT NULL,
+
+	-- SUPPLIER INFO
+	FIRST_NAME VARCHAR(64) NOT NULL,
+	LAST_NAME VARCHAR(64) NOT NULL,
+	TEL BIGINT DEFAULT NULL,
+
+	-- ADDRESS INFO
+	ADDRESS VARCHAR(128) DEFAULT NULL,
+	CITY VARCHAR(64) DEFAULT NULL,
+	ZIP INTEGER DEFAULT NULL,
+	COUNTRY VARCHAR(64) DEFAULT NULL
+);
+
+-- SUPPLIER EXAMPLES
+INSERT INTO SUPPLIER VALUES(DEFAULT, 'Juan', 'Perez', 3322554477);
+
+-- PURCHASE TABLE
+CREATE TABLE PURCHASE
+(
+	-- PRIMARY KEY
+	ID SMALLSERIAL PRIMARY KEY NOT NULL,
+
+	-- FOREIGN KEY
+	ID_SUPPLIER SMALLINT NOT NULL REFERENCES SUPPLIER(ID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+
+	-- INFO
+	DATE_PURCHASE DATE DEFAULT NOW() NOT NULL
+);
+
+-- PURCHASE EXAMPLES
+INSERT INTO PURCHASE VALUES(DEFAULT, 1, DEFAULT);
+
+-- PURCHASE_DETAIL TABLE
+CREATE TABLE PURCHASE_DETAIL
+(
+	-- PRIMARY KEY
+	ID SMALLSERIAL PRIMARY KEY NOT NULL,
+
+	-- FOREIGN KEY
+	ID_PURCHASE SMALLINT NOT NULL REFERENCES PURCHASE(ID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+
+	ID_FORMAT SMALLINT NOT NULL REFERENCES FORMAT(ID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+
+	-- INFO
+	PRICE_PURCHASE MONEY NOT NULL,
+	AMOUNT SMALLINT NOT NULL
+);
+
+-- PURCHASE_DETAIL EXAMPLES
+INSERT INTO PURCHASE_DETAIL VALUES(DEFAULT, 1, 3, 140, 25);
+INSERT INTO PURCHASE_DETAIL VALUES(DEFAULT, 1, 3, 140, 5);
